2237be6d8f2d6628691a5acb0e1f4a1848a36dfd
Ryan Johnson <ryanleejwebdev@gmail.com>
2021-06-30 11:23:06 -0500

    Set up a XMLHttpRequest to an endpoint to get a single word for handman

---
+++ async/notes.js
@@ -0,0 +1,23 @@
+// HTTP (Hypertext transfer protocol)
+// - request - what do we want to do
+// - response - what was actually done
+
+//! you can see these in the "Network" tab of your dev tools
+
+// different readyStates
+// https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/readyState
+// - 4 = complete and ready
+// --- 0-3 are rarely ever used
+
+//! Status Codes
+// https://httpstatuses.com/
+// - how a request went
+// - we can access this value via e.target.status
+// 100 - informational
+// 200 - success
+// 300 - redirection
+// 400 - client / request error
+// 500 - server error
+
+//! HTTP Messages
+// https://developer.mozilla.org/en-US/docs/Web/HTTP/Messages
--- hangman-online/app.js
+++ hangman-online/app.js
@@ -5,10 +5,31 @@ const game1 = new Hangman('Cat Food', 2)
 puzzleEl.textContent = game1.puzzle
 guessesEl.textContent = game1.statusMessage
 
-window.addEventListener('keypress', function (e) {
+window.addEventListener('keypress', (e) => {
   const guess = String.fromCharCode(e.charCode)
   game1.makeGuess(guess)
 
   puzzleEl.textContent = game1.puzzle
   guessesEl.textContent = game1.statusMessage
 })
+
+// make an http req
+const request = new XMLHttpRequest()
+request.addEventListener('readystatechange', (e) => {
+  // "e" is the request itself
+  if (e.target.readyState === 4 && e.target.status === 200) {
+    // console.log(e.target) // we get the JSON text
+    // get status
+    // console.log(e.target.status) // 200
+    const data = JSON.parse(e.target.responseText) // we parse to get a JS object
+    // console.log(data) // Object { puzzle: "Some Word" }
+  } else if (e.target.readyState === 4) {
+    console.log('an error has taken place')
+  }
+})
+// .open(http-method, url) initializes our request
+request.open('GET', 'https://puzzle.mead.io/puzzle?wordCount=3')
+// then we send of the request to initiate the process
+// - the server will takes so time for it to do what it's going to do and send back a response
+request.send()
+// - so we set up the event listener on our request wait for that response, a "readystatechange" and set it up w/ a function to run when that event happens
